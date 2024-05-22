23d36eed9cbb61d2713d2e7e9ac33a7c98606746
j <j@DESKTOP-QIDM3MF>
2024-05-22 20:30:42 +0900

    20240522 - REST ,JSON사용 RestController 작성

---
+++ ex03/src/main/java/org/zerock/domain/SampleVO.java
@@ -0,0 +1,16 @@
+package org.zerock.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor //乞窮 持失切 持失
+@NoArgsConstructor //朔 持失切 持失
+public class SampleVO {
+
+	private Integer mno;
+	private String firstName;
+	private String lastName;
+	
+}
